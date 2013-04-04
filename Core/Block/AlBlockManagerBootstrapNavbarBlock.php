ce3aedd9c52f9586816b4e59f247ebb749c2f375
alphalemon <user@server.com>
2013-04-04 06:03:36 +0200

    updated to extend from internal Menu block bundle

--- Core/Block/AlBlockManagerBootstrapNavbarBlock.php
+++ Core/Block/AlBlockManagerBootstrapNavbarBlock.php
@@ -5,59 +5,12 @@
 
 namespace AlphaLemon\Block\BootstrapNavbarBlockBundle\Core\Block;
 
-use AlphaLemon\AlphaLemonCmsBundle\Core\Content\Block\AlBlockManagerContainer;
-use AlphaLemon\AlphaLemonCmsBundle\Core\Content\Block\JsonBlock\AlBlockManagerJsonBlock;
+use AlphaLemon\Block\MenuBundle\Core\Block\AlBlockManagerMenu;
 
 /**
  * Description of AlBlockManagerBootstrapNavbarBlock
  */
-class AlBlockManagerBootstrapNavbarBlock extends AlBlockManagerContainer
+class AlBlockManagerBootstrapNavbarBlock extends AlBlockManagerMenu
 {
-    public function getDefaultValue()
-    {
-        $value = '
-            {
-                "0": {
-                    "blockType" : "Link"
-                },
-                "1": {
-                    "blockType" : "Link"
-                }
-            }';
-            
-        return array('Content' => $value);
-    }
-    
-    public function getHtml()
-    {
-        $items = AlBlockManagerJsonBlock::decodeJsonContent($this->alBlock->getContent());
-        
-        return array('RenderView' => array(
-            'view' => 'BootstrapNavbarBlockBundle:Content:navbar.html.twig',
-            'options' => array( 'items' => $items, 'parent' => $this->alBlock),
-        ));
-    }
-    
-    protected function edit(array $values)
-    {
-        $data = json_decode($values['Content'], true); 
-        $savedValues = AlBlockManagerJsonBlock::decodeJsonContent($this->alBlock);
-        
-        if ($data["operation"] == "add") {
-            $savedValues[] = $data["value"];
-            $values = array("Content" => json_encode($savedValues));
-        }
-        
-        if ($data["operation"] == "remove") {
-            unset($savedValues[$data["item"]]);
-            
-            $blocksRepository = $this->container->get('alpha_lemon_cms.factory_repository');
-            $repository = $blocksRepository->createRepository('Block');
-            $repository->deleteIncludedBlocks($data["slotName"]);
-            
-            $values = array("Content" => json_encode($savedValues));
-        }
-        
-        return parent::edit($values);
-    }
+    protected $blocksTemplate = 'BootstrapNavbarBlockBundle:Content:navbar.html.twig';    
 }
